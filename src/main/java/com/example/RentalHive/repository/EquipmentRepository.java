cad760e28123e10104b8ec878c4dbd12a4964641
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2023-11-16 13:28:08 +0100

    setup equipment needed files (repo, service)

--- src/main/java/com/example/RentalHive/repository/EquipmentRepository.java
+++ src/main/java/com/example/RentalHive/repository/EquipmentRepository.java
@@ -1,7 +1,17 @@
 package com.example.RentalHive.repository;
 
 import com.example.RentalHive.Entities.Equipment;
+import com.example.RentalHive.Entities.StatusEnum;
+import com.example.RentalHive.Entities.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface EquipmentRepository  extends JpaRepository<Equipment, Long> {
+    @Query("SELECT e FROM Equipment e WHERE e.status = :status OR e.type = :type OR e.name LIKE %:name%")
+    List<Equipment> findByStatusAndTypeAndName(@Param("status") StatusEnum status, @Param("type") Type type, @Param("name") String name);
 }
