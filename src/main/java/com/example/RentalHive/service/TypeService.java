aad2f8fe70ca09ce274b7d4bac7a2e7b438b4a74
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2023-11-17 10:57:14 +0100

    create services for entities (role, user, type)

---
+++ src/main/java/com/example/RentalHive/service/TypeService.java
@@ -0,0 +1,8 @@
+package com.example.RentalHive.service;
+
+import com.example.RentalHive.Entities.Type;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface TypeService extends CrudService<Type, Long> {
+}
--- src/main/java/com/example/RentalHive/service/imp/UserServiceImp.java
+++ src/main/java/com/example/RentalHive/service/imp/UserServiceImp.java
@@ -1,4 +1,43 @@
 package com.example.RentalHive.service.imp;
 
-public class UserServiceImp {
+import com.example.RentalHive.Entities.Users;
+import com.example.RentalHive.repository.UserRepository;
+import com.example.RentalHive.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class UserServiceImp implements UserService {
+    private final UserRepository repository;
+
+    @Override
+    public List<Users> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Users> findById(Long aLong) {
+        return repository.findById(aLong);
+    }
+
+    @Override
+    public Users save(Users entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public Users update(Users entity) {
+        return repository.save(entity);
+    }
+
+    @Override
+    public Optional<Users> delete(Long aLong) {
+        Optional<Users> user = repository.findById(aLong);
+        user.ifPresent(repository::delete);
+        return user;
+    }
 }
