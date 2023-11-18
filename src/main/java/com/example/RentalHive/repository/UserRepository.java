285f0457021e9c15c017f0d27e42a8608c726898
amine malyana <aminemalyana12@gmail.com>
2023-11-18 18:26:22 +0100

    add reservation

--- src/main/java/com/example/RentalHive/repository/UserRepository.java
+++ src/main/java/com/example/RentalHive/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.RentalHive.repository;
 
 import com.example.RentalHive.Entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository  extends JpaRepository<Users, Long> {
 }
