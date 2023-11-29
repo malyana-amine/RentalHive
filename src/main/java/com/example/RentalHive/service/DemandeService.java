a89a9bef5c8dbd175b754d8460af284a2e5ea2db
Hicham El Arfaouy <hicham.elarfaoui.1@gmail.com>
2023-11-29 13:08:51 +0100

    add other methods  (get all, get by id, update) for demand

--- src/main/java/com/example/RentalHive/service/DemandeService.java
+++ src/main/java/com/example/RentalHive/service/DemandeService.java
@@ -1,15 +1,19 @@
 package com.example.RentalHive.service;
 
+import com.example.RentalHive.DTO.DemandDTO;
 import com.example.RentalHive.entity.Demand;
-import com.example.RentalHive.entity.DemandedEquipment;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public interface DemandeService  {
 
     Demand CreateDemand(Long userId, List<Long> equipmentIds,List<LocalDate> startDateList , List<LocalDate> endDateList);
+    List<DemandDTO> findAll();
+    Optional<DemandDTO> findById(Long aLong);
+    DemandDTO update(DemandDTO entityDTO);
 }
